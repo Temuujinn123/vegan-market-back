d2cdca3bf51519bfc12e435a0a46004a91bd88ab
Temuujinn123 <temuujinn8563@gmail.com>
2024-01-23 12:07:24 +0800

    update

--- types/files.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export interface IFiles extends mongoose.Document {
-    name: string;
-    product_id: mongoose.ObjectId;
-    created_at: Date;
-}
--- types/wishlist.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export interface IWishlist extends mongoose.Document {
-    user_id?: string;
-    product_id?: string;
-    created_at: Date;
-}
