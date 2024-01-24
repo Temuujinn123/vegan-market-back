d60740529046ec9db953cb230ab6c6087080271c
Temuujinn123 <temuujinn8563@gmail.com>
2024-01-24 15:35:03 +0800

    Update

--- app/types/bannerFiles.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export interface IBannerFile extends mongoose.Document {
-    name: string;
-    created_at: Date;
-}
--- app/types/category.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export interface ICategory extends mongoose.Document {
-    name: string;
-    created_at: Date;
-    updated_at: Date;
-    is_deleted: boolean;
-}
